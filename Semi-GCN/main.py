dcbb26691748e2bbcca7f95bb075f2b0d7185593
“lw” <1284942315@qq.com>
2022-06-29 10:38:42 +0800

    update gcn project and upload coradataset

--- Semi-GCN/main.py
+++ Semi-GCN/main.py
@@ -7,14 +7,16 @@ from func import drawPlot
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def train(g,model,optimizer,loss_fn,epochs):
-    best_val_acc,best_test_acc = 0,0
+
+def train(g, model, optimizer, loss_fn, epochs):
+    best_val_acc, best_test_acc = 0, 0
     # gain features and labels of datasets
-    features,labels = g.ndata['feat'],g.ndata['label']
-    train_mask,val_mask,test_mask = g.ndata['train_mask'],g.ndata['val_mask'],g.ndata['test_mask']
-    train_acc_list,val_acc_list,test_acc_list = [],[],[]
+    features, labels = g.ndata['feat'], g.ndata['label']
+    train_mask, val_mask, test_mask = g.ndata['train_mask'], g.ndata[
+        'val_mask'], g.ndata['test_mask']
+    train_acc_list, val_acc_list, test_acc_list = [], [], []
     for e in range(epochs):
-        logits = model(g,features) # (N, label_nums)
+        logits = model(g, features)  # (N, label_nums)
         pred = logits.argmax(1)
         loss = loss_fn(logits[train_mask], labels[train_mask])
         # cal trian acc
@@ -24,24 +26,27 @@ def train(g,model,optimizer,loss_fn,epochs):
         # cal test acc
         test_acc = (pred[test_mask] == labels[test_mask]).float().mean()
 
-        train_acc_list.append(train_acc)
-        val_acc_list.append(val_acc)
-        test_acc_list.append(test_acc)
+        train_acc_list.append(train_acc.item())
+        val_acc_list.append(val_acc.item())
+        test_acc_list.append(test_acc.item())
 
         # save result based on valid dataset
         if best_val_acc < val_acc:
             best_val_acc = val_acc
             best_test_acc = test_acc
-        
+
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         if (e + 1) % 5 == 0:
-            print('Epoch {}, loss: {:.3f}, train acc: {:.3f}, val acc: {:.3f} (best {:.3f}), test acc: {:.3f} (best {:.3f})'.format(
-                e + 1, loss, train_acc, val_acc, best_val_acc, test_acc, best_test_acc))
-        
-    drawPlot([train_acc_list,val_acc_list,test_acc_list],"accuracy.png","Acc",["train","val","test"])
+            print(
+                'Epoch {}, loss: {:.3f}, train acc: {:.3f}, val acc: {:.3f} (best {:.3f}), test acc: {:.3f} (best {:.3f})'
+                .format(e + 1, loss, train_acc, val_acc, best_val_acc,
+                        test_acc, best_test_acc))
+    drawPlot([train_acc_list, val_acc_list, test_acc_list], "accuracy.png",
+             "Acc", ["train", "val", "test"])
+
 
 if __name__ == "__main__":
     epochs = 200
@@ -49,7 +54,7 @@ if __name__ == "__main__":
     lr = 0.01
     weight_decay = 5e-4
     # download and loading dataset
-    dataset = dgl.data.CoraGraphDataset(raw_dir="../Datasets/")
+    dataset = dgl.data.CoraGraphDataset(raw_dir="../Datasets/DGL/")
     g = dataset[0]
     # add self-loop
     g = dgl.remove_self_loop(g)
@@ -61,18 +66,16 @@ if __name__ == "__main__":
     norm[torch.isinf(norm)] = 0
     g.ndata['norm'] = norm.unsqueeze(1)
 
-    model = GCNModel(
-        in_feats=g.ndata['feat'].shape[1],
-        h_feats=hidden_size,
-        num_classes=dataset.num_classes
-        )
-    
+    model = GCNModel(in_feats=g.ndata['feat'].shape[1],
+                     h_feats=hidden_size,
+                     num_classes=dataset.num_classes)
+
     if torch.cuda.is_available():
         g = g.to(device)
         model = model.to(device)
 
-    optimizer = optim.Adam(model.parameters(),lr=lr,weight_decay=weight_decay)
+    optimizer = optim.Adam(model.parameters(),
+                           lr=lr,
+                           weight_decay=weight_decay)
     loss_fn = nn.CrossEntropyLoss()
-    train(g,model,optimizer,loss_fn,epochs)
-    
-
+    train(g, model, optimizer, loss_fn, epochs)
